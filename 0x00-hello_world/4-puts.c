ac372361d89f69ade3b5c87a03b33c5d314fcc3d
albert-ui <albertmkiugi@gmail.com>
2022-09-09 06:47:50 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,37 @@
-1 /*
-2     *my  first c program
-3     *file 4-puts.c
-4     */
-5
-6    #iclude <stdio.h>
-7    
-8     int main (void)
-9    {
-10       puts("\"Programming is like building a multilingual puzzle");
-11       retun(0)
-12    }
+
+/*
+ *
+ *  * 4-puts.c
+ *
+ *  *my first c code>
+ *
+ *  */
+
+
+
+#include <stdio.h>
+
+
+
+/**
+ *
+ *  * main - Prints "Programming is like building a multilingual
+ *
+ *  *                puzzle, followed by a new line.
+ *
+ *  *
+ *
+ *  * Return: Always 0.
+ *
+ *  */
+
+int main(void)
+
+{
+
+		puts("\"Programming is like building a multilingual puzzle");
+
+
+
+			return (0);
+}
