9584a56c0cb5634271609b56d44c45174dfde1cd
albert-ui <albertmkiugi@gmail.com>
2022-09-09 06:05:54 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+1 /*
+2     *my  first c program
+3     *file 4-puts.c
+4     */
+5
+6    #iclude <stdio.h>
+7    
+8     int main (void)
+9    {
+10       puts("\"Programming is like building a multilingual puzzle");
+11       retun(0)
+12    }
